01ac98b2bda46d07197402433894e8336dae500b
erichkko <erichkko@163.com>
2016-11-08 20:35:56 +0800

    Initial Commit

---
+++ SegmentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentDemo
+//
+//  Created by WangLong on 16/11/8.
+//  Copyright © 2016年 WangLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegmentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SegmentDemo
+//
+//  Created by WangLong on 16/11/8.
+//  Copyright © 2016年 WangLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
